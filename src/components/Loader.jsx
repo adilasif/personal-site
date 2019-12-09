ca7aa33c711a2081a86ea3d08f9d956e18b7699a
Adil Asif <adil@adilasif.com>
2019-12-09 03:47:58 -0500

    Create loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import anime from 'animejs';
+
+import { LogoLoader } from '@components/icons';
+import { theme, mixins } from '@styled-components';
+
+const { colors } = theme;
+
+const StyledContainer = styled.div`
+  ${mixins.flexCenter};
+  background-color: ${colors.darkBackground};
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 99;
+`;
+
+const StyledLogo = styled.div`
+  width: max-content;
+  max-width: 100px;
+  transition: ${theme.transition};
+  opacity: ${props => (props.isMounted ? 1 : 0)};
+  svg {
+    width: 100%;
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+    fill: none;
+    user-select: none;
+    #AA {
+      opacity: 0;
+    }
+  }
+`;
+
+const Loader = ({ finishLoading }) => {
+  const animate = () => {
+    const loader = anime.timeline({
+      complete: () => finishLoading(),
+    });
+
+    loader
+      .add({
+        targets: '#logo #outline',
+        delay: 500,
+        duration: 2000,
+        easing: 'easeInOutQuart',
+        strokeDashoffset: [anime.setDashoffset, 2525],
+      })
+      .add({
+        targets: '#logo #AA',
+        duration: 800,
+        easing: 'easeInOutQuart',
+        opacity: 1,
+      })
+      .add({
+        targets: '#logo',
+        delay: 700,
+        duration: 300,
+        easing: 'easeInOutQuart',
+        opacity: 0,
+        scale: 0.1,
+      })
+      .add({
+        targets: '.loader',
+        duration: 200,
+        easing: 'easeInOutQuart',
+        opacity: 0,
+        zIndex: -1,
+      });
+  };
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 10);
+    animate();
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <StyledContainer className="loader">
+      <Helmet bodyAttributes={{ class: `hidden` }} />
+
+      <StyledLogo isMounted={isMounted}>
+        <LogoLoader />
+      </StyledLogo>
+    </StyledContainer>
+  );
+};
+
+Loader.propTypes = {
+  finishLoading: PropTypes.func.isRequired,
+};
+
+export default Loader;
