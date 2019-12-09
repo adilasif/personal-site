bc757c0948eeba24f84e1b07518307bc0c94ad6b
Adil Asif <adil@adilasif.com>
2019-12-09 03:45:30 -0500

    Add global styles and theming; Add initial styled components

---
+++ src/styled-components/fonts.js
@@ -0,0 +1,88 @@
+import CalibreLightTTF from '@assets/Calibre/Calibre-Light.ttf';
+import CalibreLightWOFF from '@assets/Calibre/Calibre-Light.woff';
+import CalibreLightWOFF2 from '@assets/Calibre/Calibre-Light.woff2';
+import CalibreLightItalicTTF from '@assets/Calibre/Calibre-LightItalic.ttf';
+import CalibreLightItalicWOFF from '@assets/Calibre/Calibre-LightItalic.woff';
+import CalibreLightItalicWOFF2 from '@assets/Calibre/Calibre-LightItalic.woff2';
+import CalibreRegularTTF from '@assets/Calibre/Calibre-Regular.ttf';
+import CalibreRegularWOFF from '@assets/Calibre/Calibre-Regular.woff';
+import CalibreRegularWOFF2 from '@assets/Calibre/Calibre-Regular.woff2';
+import CalibreRegularItalicTTF from '@assets/Calibre/Calibre-RegularItalic.ttf';
+import CalibreRegularItalicWOFF from '@assets/Calibre/Calibre-RegularItalic.woff';
+import CalibreRegularItalicWOFF2 from '@assets/Calibre/Calibre-RegularItalic.woff2';
+import CalibreMediumTTF from '@assets/Calibre/Calibre-Medium.ttf';
+import CalibreMediumWOFF from '@assets/Calibre/Calibre-Medium.woff';
+import CalibreMediumWOFF2 from '@assets/Calibre/Calibre-Medium.woff2';
+import CalibreMediumItalicTTF from '@assets/Calibre/Calibre-MediumItalic.ttf';
+import CalibreMediumItalicWOFF from '@assets/Calibre/Calibre-MediumItalic.woff';
+import CalibreMediumItalicWOFF2 from '@assets/Calibre/Calibre-MediumItalic.woff2';
+import CalibreSemiboldTTF from '@assets/Calibre/Calibre-Semibold.ttf';
+import CalibreSemiboldWOFF from '@assets/Calibre/Calibre-Semibold.woff';
+import CalibreSemiboldWOFF2 from '@assets/Calibre/Calibre-Semibold.woff2';
+import CalibreSemiboldItalicTTF from '@assets/Calibre/Calibre-SemiboldItalic.ttf';
+import CalibreSemiboldItalicWOFF from '@assets/Calibre/Calibre-SemiboldItalic.woff';
+import CalibreSemiboldItalicWOFF2 from '@assets/Calibre/Calibre-SemiboldItalic.woff2';
+
+import SFMonoRegularTTF from '@assets/SFMono/SFMono-Regular.ttf';
+import SFMonoRegularWOFF from '@assets/SFMono/SFMono-Regular.woff';
+import SFMonoRegularWOFF2 from '@assets/SFMono/SFMono-Regular.woff2';
+import SFMonoRegularItalicTTF from '@assets/SFMono/SFMono-RegularItalic.ttf';
+import SFMonoRegularItalicWOFF from '@assets/SFMono/SFMono-RegularItalic.woff';
+import SFMonoRegularItalicWOFF2 from '@assets/SFMono/SFMono-RegularItalic.woff2';
+import SFMonoMediumTTF from '@assets/SFMono/SFMono-Medium.ttf';
+import SFMonoMediumWOFF from '@assets/SFMono/SFMono-Medium.woff';
+import SFMonoMediumWOFF2 from '@assets/SFMono/SFMono-Medium.woff2';
+import SFMonoMediumItalicTTF from '@assets/SFMono/SFMono-MediumItalic.ttf';
+import SFMonoMediumItalicWOFF from '@assets/SFMono/SFMono-MediumItalic.woff';
+import SFMonoMediumItalicWOFF2 from '@assets/SFMono/SFMono-MediumItalic.woff2';
+import SFMonoSemiboldTTF from '@assets/SFMono/SFMono-Semibold.ttf';
+import SFMonoSemiboldWOFF from '@assets/SFMono/SFMono-Semibold.woff';
+import SFMonoSemiboldWOFF2 from '@assets/SFMono/SFMono-Semibold.woff2';
+import SFMonoSemiboldItalicTTF from '@assets/SFMono/SFMono-SemiboldItalic.ttf';
+import SFMonoSemiboldItalicWOFF from '@assets/SFMono/SFMono-SemiboldItalic.woff';
+import SFMonoSemiboldItalicWOFF2 from '@assets/SFMono/SFMono-SemiboldItalic.woff2';
+
+export {
+  CalibreLightTTF,
+  CalibreLightWOFF,
+  CalibreLightWOFF2,
+  CalibreLightItalicTTF,
+  CalibreLightItalicWOFF,
+  CalibreLightItalicWOFF2,
+  CalibreRegularTTF,
+  CalibreRegularWOFF,
+  CalibreRegularWOFF2,
+  CalibreRegularItalicTTF,
+  CalibreRegularItalicWOFF,
+  CalibreRegularItalicWOFF2,
+  CalibreMediumTTF,
+  CalibreMediumWOFF,
+  CalibreMediumWOFF2,
+  CalibreMediumItalicTTF,
+  CalibreMediumItalicWOFF,
+  CalibreMediumItalicWOFF2,
+  CalibreSemiboldTTF,
+  CalibreSemiboldWOFF,
+  CalibreSemiboldWOFF2,
+  CalibreSemiboldItalicTTF,
+  CalibreSemiboldItalicWOFF,
+  CalibreSemiboldItalicWOFF2,
+  SFMonoRegularTTF,
+  SFMonoRegularWOFF,
+  SFMonoRegularWOFF2,
+  SFMonoRegularItalicTTF,
+  SFMonoRegularItalicWOFF,
+  SFMonoRegularItalicWOFF2,
+  SFMonoMediumTTF,
+  SFMonoMediumWOFF,
+  SFMonoMediumWOFF2,
+  SFMonoMediumItalicTTF,
+  SFMonoMediumItalicWOFF,
+  SFMonoMediumItalicWOFF2,
+  SFMonoSemiboldTTF,
+  SFMonoSemiboldWOFF,
+  SFMonoSemiboldWOFF2,
+  SFMonoSemiboldItalicTTF,
+  SFMonoSemiboldItalicWOFF,
+  SFMonoSemiboldItalicWOFF2,
+};
---
+++ src/styled-components/index.js
@@ -0,0 +1,11 @@
+import theme from './theme';
+import media from './media';
+import mixins from './mixins';
+
+import GlobalStyle from './GlobalStyle';
+import Header from './Header';
+import Main from './Main';
+import Section from './Section';
+import Heading from './Heading';
+
+export { theme, media, mixins, GlobalStyle, Header, Main, Section, Heading };
