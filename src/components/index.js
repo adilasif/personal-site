a1e5057b803adc6dc60e6b24009d73825563eec6
Adil Asif <adil@adilasif.com>
2020-01-20 03:23:33 -0500

    Add component exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,27 @@
 import Head from './Head';
 import Layout from './Layout';
 import Loader from './Loader';
+import Nav from './Nav';
+import Social from './Social';
+import Email from './Email';
 import Hero from './Hero';
+import Menu from './Menu';
+import About from './About';
+import Featured from './Featured';
+import Contact from './Contact';
+import Footer from './Footer';
 
-export { Head, Layout, Loader, Hero };
+export {
+  Head,
+  Layout,
+  Loader,
+  Nav,
+  Social,
+  Email,
+  Hero,
+  Menu,
+  About,
+  Featured,
+  Contact,
+  Footer,
+};
