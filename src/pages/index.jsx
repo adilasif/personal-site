27cf43a80cd822f98a68671986bb9521870ca6a5
Adil Asif <adil@adilasif.com>
2020-01-20 03:24:02 -0500

    Update main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { Layout, Hero } from '@components';
+import { Layout, Hero, About, Featured, Contact } from '@components';
 import { Main } from '@styled-components';
 
 const StyledMainContainer = styled(Main)`
@@ -14,6 +14,9 @@ const IndexPage = ({ location, data }) => (
   <Layout location={location}>
     <StyledMainContainer>
       <Hero data={data.hero.edges} />
+      <About data={data.about.edges} />
+      <Featured data={data.featured.edges} />
+      <Contact data={data.contact.edges} />
     </StyledMainContainer>
   </Layout>
 );
@@ -33,6 +36,68 @@ export const pageQuery = graphql`
             name
             subtitle
             contactText
+          }
+          html
+        }
+      }
+    }
+    about: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/about/" } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            avatar {
+              childImageSharp {
+                fluid(
+                  maxWidth: 700
+                  quality: 90
+                  traceSVG: { color: "#FF6600" }
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
+            }
+            skills
+          }
+          html
+        }
+      }
+    }
+    featured: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/featured/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            cover {
+              childImageSharp {
+                fluid(
+                  maxWidth: 700
+                  quality: 90
+                  traceSVG: { color: "#64ffda" }
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
+            }
+            tech
+            show
+          }
+          html
+        }
+      }
+    }
+    contact: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/contact/" } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
           }
           html
         }
