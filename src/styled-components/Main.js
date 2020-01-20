f80be187448d629ae453e5ff11c4951855e78f84
Adil Asif <adil@adilasif.com>
2020-01-20 03:18:41 -0500

    Update Main styled component

--- src/styled-components/Main.js
+++ src/styled-components/Main.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 import mixins from './mixins';
+import theme from './theme';
 
 const Main = styled.main`
   ${mixins.sidePadding};
   margin: 0 auto;
   width: 100%;
+  background-color: ${theme.colors.background};
 `;
 
 export default Main;
