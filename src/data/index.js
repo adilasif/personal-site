90f42c49242140a7152a21a784a96e9b65d66a3f
Adil Asif <adil@adilasif.com>
2020-01-20 02:57:55 -0500

    Update gatsby data file

--- src/data/index.js
+++ src/data/index.js
@@ -10,9 +10,54 @@ module.exports = {
   location: 'Chapel Hill, NC',
   email: 'adil@adilasif.com',
   github: 'https://github.com/adilasif',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/adilasif',
+    },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/adilaasif',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/functionalCOBOL',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
 
   navHeight: 100,
   primaryColor: '#FF6600',
   backgroundColor: '#28293D',
   backgroundDarkColor: '#1C1C28',
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
 };
