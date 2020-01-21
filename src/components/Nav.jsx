a6e012112023285b59cd2a292424d099ce4cc8f8
Adil Asif <adil@adilasif.com>
2020-01-21 02:07:29 -0500

    Add resume button to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -307,7 +307,7 @@ class Nav extends Component {
               </TransitionGroup>
             </StyledList>
 
-            {/* <TransitionGroup component={null}>
+            <TransitionGroup component={null}>
               {isMounted && (
                 <CSSTransition classNames="fadedown" timeout={3000}>
                   <div style={{ transitionDelay: `600ms` }}>
@@ -321,7 +321,7 @@ class Nav extends Component {
                   </div>
                 </CSSTransition>
               )}
-            </TransitionGroup> */}
+            </TransitionGroup>
           </StyledLink>
         </StyledNav>
 
